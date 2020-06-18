5d536dd11c4ce9efcb1735d33f38aeae88a55cc5
ygq <493650029@qq.com>
2020-06-18 22:37:13 +0800

    ygq-MarsCar

---
+++ src/test/java/com/twschool/practice/MarsCarTest.java
@@ -0,0 +1,163 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MarsCarTest {
+    @Test
+    public void should_return_00W_when_commend_L_given_00N(){
+        //given
+        Position position = new Position(0,0,"N");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "L";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)W",result);
+    }
+
+    @Test
+    public void should_return_00E_when_commend_R_given_00N(){
+        //given
+        Position position = new Position(0,0,"N");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "R";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)E",result);
+    }
+
+    @Test
+    public void should_return_01N_when_commend_M_given_00N(){
+        //given
+        Position position = new Position(0,0,"N");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "M";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,1)N",result);
+    }
+
+    @Test
+    public void should_return_f10W_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "M";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(-1,0)W",result);
+    }
+
+    @Test
+    public void should_return_00S_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "L";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)S",result);
+    }
+
+    @Test
+    public void should_return_00N_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "R";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)N",result);
+    }
+
+    @Test
+    public void should_return_0f1S_when_commend_M_given_00S(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "M";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(-1,0)W",result);
+    }
+
+    @Test
+    public void should_return_00E_when_commend_L_given_00S() {
+        //given
+        Position position = new Position(0, 0, "S");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "L";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)E", result);
+    }
+
+    @Test
+    public void should_return_00W_when_commend_R_given_00S() {
+        //given
+        Position position = new Position(0, 0, "S");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "R";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)W", result);
+    }
+
+    @Test
+    public void should_return_10E_when_commend_M_given_00E() {
+        //given
+        Position position = new Position(0, 0, "E");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "M";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(1,0)E", result);
+    }
+
+    @Test
+    public void should_return_00N_when_commend_L_given_00E() {
+        //given
+        Position position = new Position(0, 0, "E");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "L";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)N", result);
+    }
+
+    @Test
+    public void should_return_00S_when_commend_R_given_00E() {
+        //given
+        Position position = new Position(0, 0, "E");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "R";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)S", result);
+    }
+
+    @Test
+    public void should_return_01N_when_commend_MRL_given_00N() {
+        //given
+        Position position = new Position(0, 0, "N");
+        MarsCar marsCar = new MarsCar(position);
+        String str = "MRL";
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,1)N", result);
+    }
+}
