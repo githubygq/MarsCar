098410105923d3846019e2a6712c5261994287d7
ygq <493650029@qq.com>
2020-06-18 23:16:26 +0800

    [ygq] should_return_f10W_when_commend_M_given_00W() [ygq] should_return_00S_when_commend_M_given_00W() [ygq] should_return_00N_when_commend_M_given_00W()

--- src/test/java/com/twschool/practice/MarsCarTest.java
+++ src/test/java/com/twschool/practice/MarsCarTest.java
@@ -45,4 +45,46 @@ public class MarsCarTest {
         //then
         assertEquals("(0,1)N",result);
     }
+
+    @Test
+    public void should_return_f10W_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+
+        String str = "M";
+        //List<String> commd = Arrays.asList(str);
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(-1,0)W",result);
+    }
+
+    @Test
+    public void should_return_00S_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+
+        String str = "L";
+        //List<String> commd = Arrays.asList(str);
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)S",result);
+    }
+
+    @Test
+    public void should_return_00N_when_commend_M_given_00W(){
+        //given
+        Position position = new Position(0,0,"W");
+        MarsCar marsCar = new MarsCar(position);
+
+        String str = "R";
+        //List<String> commd = Arrays.asList(str);
+        //when
+        String result = marsCar.move(str);
+        //then
+        assertEquals("(0,0)N",result);
+    }
 }
